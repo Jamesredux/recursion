3c25279f5ca20cb53001f5a8331a2a78e10d19bb
James McCann <jamesredux@gmail.com>
2017-09-12 19:50:14 +0700

    fibonacci problem completed

---
+++ fibonacci.rb
@@ -0,0 +1,60 @@
+def fibs(n)
+	result = []
+	if n == 0  
+		result<<n
+	elsif n == 1
+		result<<0 <<1
+				
+		
+	else
+		result = [0,1]
+		a = 0
+		b = 1
+		(n-1).times do 
+			a += b
+			result<<a
+			a,b = b,a
+
+			
+		end
+
+	end	
+ puts result.inspect
+ 
+end
+
+def fibs_recur(n)   #simple solve using recursion
+
+		return 0 if n == 0
+		return 1 if n == 1
+					
+		fibs_recur(n-1) + fibs_recur(n-2)
+		
+end
+
+def fibs_recur_list(n)
+	list =  []
+	for x in 0..n
+		list<<fibs_recur(x)
+		
+	end
+	list.inspect
+end
+
+
+#tests
+
+fibs(0)
+fibs(1)
+fibs(2)
+fibs(5)
+
+puts fibs_recur(0)
+puts fibs_recur(1)
+puts fibs_recur(2)
+puts fibs_recur(5)
+
+puts fibs_recur_list(0)
+puts fibs_recur_list(1)
+puts fibs_recur_list(2)
+puts fibs_recur_list(5)
