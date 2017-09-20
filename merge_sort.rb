8dcc9a960ea133e135c591f9a67ed7e168f8d737
James McCann <jamesredux@gmail.com>
2017-09-20 17:35:41 +0700

    final commit

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,56 +1,34 @@
 
-
 def merge_sort(input)
 	
 	n = input.size
 	y = input.size/2
 	
-	if n < 2
+	if n <= 1
 		return input
-	else	
-		left = input[0...y] #these two lines split the array
-		right = input[y..-1]
-		
-		merge_sort(left) 
-		merge_sort(right)		
-
-		merge(left,right, input)
+	else			
+		left = merge_sort(input[0...y])
+		right = merge_sort(input[y..-1])
+		merge(left,right)
 	end 
-	input
+	
 end	
 
 
+def merge(left,right)
+	array = []
+	until left.empty? || right.empty?
+		if left.first <= right.first
+			array<<left.shift
+		else
+			array<<right.shift
+		end
+	end
+		array += left += right		
+end
 
 
-def merge(left,right,a)
-		a_pointer = 0 
-		a_end = a.size - 1
-		left_pointer = 0
-		left_end = left.size - 1
-		right_pointer = 0
-		right_end = right.size - 1
-
-			while left_pointer <= left_end && right_pointer <= right_end
-				if left[left_pointer] < right[right_pointer]
-					a[a_pointer] = left[left_pointer]
-					left_pointer += 1
-				else
-					a[a_pointer] = right[right_pointer]
-					right_pointer += 1
-				end
-					a_pointer +=1	
-			end
-
-			if left_pointer == left_end + 1
-				a[a_pointer..a_end] = right[right_pointer..right_end]		
-			else
-				a[a_pointer..a_end] = left[left_pointer..left_end]
-			end	
-		
-end		
-
-
-puts merge_sort([7,9,1,4,8])	
+puts merge_sort([6,3,4,1,9,2,7,5,8]).inspect
 
 
 
